7c96ca2d8fe8de4f0936f0b4d5e9841ecccba445
Sonycodes <soniatavaresebirdll@gmail.com>
2023-11-24 10:49:49 +0100

    test carrousel + image orga

---
+++ Naruto perso/script.js
@@ -0,0 +1,12 @@
+
+function changerLangue() {
+    var selectLangue = document.getElementById("langue");
+    var message = document.getElementById("message");
+
+    // // Changer le contenu en fonction de la langue sélectionnée
+    // if (selectLangue.value === "fr") {
+    //     message.innerHTML = "Bienvenue sur notre site !";
+    // } else if (selectLangue.value === "en") {
+    //     message.innerHTML = "Welcome to our website!";
+    // }
+}
